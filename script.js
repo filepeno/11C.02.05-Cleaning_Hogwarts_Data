88c96999d9c549a2dbf6e0c968652da070c5ff59
Filip <fili4252@stud.kea.dk>
2021-09-05 23:54:04 +0200

    got and formatted firstnames, found middle and nicknames

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
 "use strict";
+
+const Student = {
+  firstName: "",
+  lastName: "",
+  middleName: "",
+  nickName: "",
+  image: "",
+  house: "",
+};
+
+window.addEventListener("DOMContentLoaded", start);
+
+function start() {
+  console.log("ready");
+  loadJSON();
+}
+
+function loadJSON() {
+  fetch("students.json")
+    .then((response) => response.json())
+    .then((jsonData) => {
+      // when loaded, prepare objects
+      prepareObjects(jsonData);
+    });
+}
+
+function prepareObjects(students) {
+  console.log("prepareObjects()");
+  students.forEach((student) => {
+    // console.log(student);
+    const firstName = getFirstName(student.fullname);
+    const middleName = findMiddleOrNickName(student.fullname);
+    // console.log(firstName);
+    // console.log(middleName);
+  });
+  const student = Object.create(Student);
+}
+
+function findMiddleOrNickName(fullname) {
+  const trimmedName = fullname.trim().toLowerCase();
+  const nameCount = trimmedName.split(" ");
+  console.log(nameCount.length);
+  if (nameCount.length > 2) {
+    const middleNameOrNickName = nameCount[1];
+    getMiddleNameOrNickName(middleNameOrNickName);
+  } else {
+    return null;
+  }
+}
+
+function getFirstName(fullname) {
+  const trimmedName = fullname.trim().toLowerCase();
+  const firsNameEnd = trimmedName.indexOf(" ");
+  const firstNameCaps = trimmedName[0].toUpperCase() + trimmedName.substring(1, firsNameEnd);
+  return firstNameCaps;
+}
+
+function getMiddleNameOrNickName(mid) {
+  console.log(mid);
+  //   const middleNameCaps = trimmedName[0].toUpperCase() + trimmedName.substring(1, firsNameEnd);
+  //   return middleNameCaps;
+}
